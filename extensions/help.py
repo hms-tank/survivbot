7879ad0b47894bb8338fd88f99e467002bfcfae2
hms <72023614+hms-tank@users.noreply.github.com>
2023-03-30 11:12:48 -0400

    move help command to help.py

---
+++ extensions/help.py
@@ -0,0 +1,53 @@
+import discord
+from discord.ext import commands
+
+class Help(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @client.command()
+    async def help(ctx):
+        embed = discord.Embed(title="help", color=0xFF0000)
+        embed.add_field(
+            name="$explain",
+            value="Explains this server and lists FAQ."
+        )
+        embed.add_field(
+            name="$links",
+            value="Lists various links related to the project."
+        )
+        embed.add_field(
+            name="$serverstatus",
+            value=(
+                "Checks whether the game server (or at least website) is up. "
+                "It checks all websites on which the game is hosted. "
+                "Not too reliable, might return 502 errors."
+            )
+        )
+        embed.add_field(
+            name="$getxp",
+            value="This command shows the amount of XP the sender has."
+        )
+        embed.add_field(
+            name="$getleaderboard",
+            value=(
+                "This command lists the 5 members of the server "
+                "with the most XP!. (6 including bot)"
+            )
+        )
+        embed.add_field(
+            name="$checkurl",
+            value="Checks the connectivity of any URL."
+        )
+        embed.add_field(
+            name="$help",
+            value="This command."
+        )
+        embed.add_field(
+            name="Bot GitHub",
+            value="https://github.com/Killaship/survivbot"
+        )
+        await ctx.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Help(bot))
--- main.py
+++ main.py
@@ -89,17 +89,6 @@ async def shell(ctx,cmd):
     
     
   
-@client.command()
-async def links(ctx):
-    embed = discord.Embed(title="Links", description="", color=0xFF0000)
-    embed.add_field(name="Test Server", value="https://test.resurviv.io")
-    embed.add_field(name="Play Stable Version", value="https://resurviv.io")
-    embed.add_field(name="Discord Perma Invite", value="https://discord.resurviv.io")
-    embed.add_field(name="Subreddit", value="https://reddit.com/r/survivreloaded")
-    embed.add_field(name="Github (Organization)", value="https://github.com/SurvivReloaded")
-    embed.add_field(name="GitLab (deprecated)", value="https://gitlab.com/hasanger/survivreloaded")
-    embed.add_field(name="Bot GitHub", value="https://github.com/Killaship/survivbot")
-    await ctx.send(embed=embed)
 
 
 # Killaship told me that this command is deprecated.
@@ -170,32 +159,6 @@ async def links(ctx):
 
 
 
-@client.command()
-async def help(ctx):
-   
-  
-
-    embed = discord.Embed(title="help", description="", color=0xFF0000)#Declaring the help command is an embed.
-
-    
-
-    embed.add_field(name="$explain", value="Explains this server and lists FAQ.")
-
-    embed.add_field(name="$links", value="Lists various links related to the project.")
-
-    embed.add_field(name="$serverstatus", value="Checks whether the game server (or at least website) is up. It checks all websites on which the game is hosted. Not too reliable, might return 502 errors.")
-
-    embed.add_field(name="$getxp", value="This command shows the amount of XP the sender has.")
-    
-    embed.add_field(name="$getleaderboard", value="This command lists the 5 members of the server with the most XP!. (6 including bot)")
-
-    embed.add_field(name="$checkurl", value="Checks the connectivity of any URL.")
-
-    embed.add_field(name="$help", value="This command.")
-
-    embed.add_field(name="Bot GitHub", value="https://github.com/Killaship/survivbot")
-
-    await ctx.send(embed=embed)#sends the embed.
 
 
 @client.command()
